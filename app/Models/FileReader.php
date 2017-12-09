690f3968038df095b54f20616aa9677b5e5ff427
Fedchishina Victoria <fedchishina.victoria@gmail.com>
2017-12-09 15:01:05 +0300

    added comments

--- app/Models/FileReader.php
+++ app/Models/FileReader.php
@@ -3,7 +3,9 @@
 
 class FileReader implements SeekableIterator
 {
+    //position in file
     private $position;
+    // file line array
     private $fileArray;
 
     public function __construct($file)
@@ -11,32 +13,50 @@ class FileReader implements SeekableIterator
         $this->position = 0;
         $this->fileArray = file($file);
     }
-
+    
+    /*
+     * get current line of file
+     */
     public function current()
     {
         return $this->fileArray[$this->position];
     }
-
+    
+    /*
+     * increase the current file position
+     */
     public function next()
     {
         ++$this->position;
     }
 
+    /*
+     * get current position of file
+     */
     public function key()
     {
         return $this->position;
     }
 
+    /*
+     * check for the existence of the file position
+     */
     public function valid()
     {
         return isset($this->array[$this->position]);
     }
 
+    /*
+     * make position of file is 0 (start position)
+     */ 
     public function rewind()
     {
         $this->position = 0;
     }
-
+    
+    /*
+     * change position of file
+     */ 
     public function seek($position)
     {
         if (!isset($this->fileArray[$position])) {
